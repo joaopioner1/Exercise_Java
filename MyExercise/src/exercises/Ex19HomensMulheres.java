cb8ba39575eef954fe2c869e616aac585fbe6b84
João Vítor Souza Pioner <66327190+joaopioner1@users.noreply.github.com>
2020-06-23 19:39:42 -0300

    Ajeitei o exercício.

--- MyExercise/src/exercises/Ex19HomensMulheres.java
+++ MyExercise/src/exercises/Ex19HomensMulheres.java
@@ -19,6 +19,7 @@ public class Ex19HomensMulheres {
 		if (idadeH1 > idadeH2 && idadeM1 < idadeM2) {
 			int totalH1M1 = idadeH1 + idadeM1;
 			int totalH2M2 = idadeH2 + idadeM2;
+			
 			System.out.println("A idade da mulher mais velha é: "+idadeM2);
 			System.out.println("A idade da mulher mais nova é: "+idadeM1);
 			System.out.println("A idade do homem mais velho é: "+idadeH1);
@@ -29,6 +30,7 @@ public class Ex19HomensMulheres {
 		else if (idadeH1 < idadeH2 && idadeM2 > idadeM1) {
 			int totalH2M1 = idadeH2 + idadeM1;
 			int totalH1M2 = idadeH1 + idadeM2;
+			
 			System.out.println("A idade da mulher mais velha é: "+idadeM1);
 			System.out.println("A idade da mulher mais nova é: "+idadeM2);
 			System.out.println("A idade do homem mais velho é: "+idadeH2);
@@ -39,6 +41,7 @@ public class Ex19HomensMulheres {
 		else if (idadeH1 < idadeH2 && idadeM1 < idadeM2) {
 			int totalH2M2 = idadeH2 + idadeM2;
 			int totalH1M1 = idadeH1 + idadeM1;
+			
 			System.out.println("A idade da mulher mais velha é: "+idadeM2);
 			System.out.println("A idade da mulher mais nova é: "+idadeM1);
 			System.out.println("A idade do homem mais velho é: "+idadeH2);
