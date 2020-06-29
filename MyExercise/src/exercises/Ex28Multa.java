ed3db5df19fa7f71f2acdb24a90c6aff922d3220
João Vítor Souza Pioner <joaopioner@outlook.com>
2020-06-28 22:59:16 -0300

    Adicionei mais exercícios.

---
+++ MyExercise/src/exercises/Ex28Multa.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Ex28Multa {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Insira o valor da multa: ");
+		double valorMulta = in.nextDouble();
+		System.out.println("Insira o mês(em número) que a multa foi paga: ");
+		int mes = in.nextInt();
+
+		double juroComp = valorMulta * (Math.pow(1 + 0.05, mes));
+		if (mes == 1) {
+			System.out.printf("O valor da multa a ser pago é: %.2f", valorMulta);
+		} else {
+			while (mes >= 2 && mes <= 12) {
+				System.out.printf("O valor da multa a ser pago é: %.2f", juroComp);
+				break;
+			}
+		}
+
+		in.close();
+	}
+}
+/*
+ * 3) Criar um programa para calcular o valor da multa a ser paga de anuidade de
+ * uma associação. A anuidade deve ser paga no mês de janeiro. Por mês, é
+ * cobrado 5% de juros (com juros sobre juros). Por exemplo, uma associação de
+ * R$100 paga em janeiro, custa R$ 100; em fevereiro, custa R$105; em março,
+ * custa R$110,25; e, em dezembro, R$171,03. O usuário deve informar o valor da
+ * multa o mês em que foi paga.
+ */
---
+++ MyExercise/src/exercises/Ex30Mesada.java
@@ -0,0 +1,52 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Ex30Mesada {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Quanto você recebe de mesada? ");
+		double mesada = in.nextDouble();
+		System.out.println("Você deseja comprar alguma coisa?(sim/nao) ");
+		String resposta = in.next();
+
+		if (resposta.equals("nao")) {
+			System.out.println("Tudo bem, até logo!");
+		}
+		if (resposta.equals("sim")) {
+			System.out.println("Insira o valor do produto que deseja comprar: ");
+			double valorP1 = in.nextDouble();
+
+			if (valorP1 <= mesada) {
+				System.out.println(
+						"Você pode comprar esse produto, e o saldo restante é: " + (mesada - valorP1 + " reais."));
+				double resto = mesada - valorP1;
+
+				System.out.println("Você deseja comprar mais alguma coisa?(sim/nao) ");
+				String resposta2 = in.next();
+
+				if (resposta2.equals("sim")) {
+					System.out.println("Insira o valor do produto que deseja comprar: ");
+					double valorP2 = in.nextDouble();
+
+					if (resto >= valorP2) {
+						System.out.println("Você pode comprar somente mais esse produto, porque o estoque da loja acabou. O saldo restante é: " + (resto - valorP2) + " reais.");
+					} else {
+						System.out.println("Você não tem dinheiro para comprar esse produto.");
+					}
+				}
+			}
+			in.close();
+		}
+
+	}
+}
+
+/*
+ * 4) Faça um programa que: pergunte a mesada de um adolescente. Após isso
+ * pergunte se ele deseja comprar alguma coisa. O adolescente pode comprar
+ * produtos até o dinheiro acabar ou pode comprar somente o necessário, ficando
+ * com saldo restante
+ */
