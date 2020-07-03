bbc885dcc9d944135e6fc4f5e2996dbde86bf443
João Vítor Souza Pioner <66327190+joaopioner1@users.noreply.github.com>
2020-07-02 21:19:47 -0300

    Tirei o ln do print.

--- MyExercise/src/exercises/Ex32AumentoSalario.java
+++ MyExercise/src/exercises/Ex32AumentoSalario.java
@@ -7,33 +7,33 @@ public class Ex32AumentoSalario {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
-		System.out.println("Insira o sal·rio do colaborador: ");
+		System.out.print("Insira o sal√°rio do colaborador: ");
 		double salario = in.nextDouble();
 
 		if (salario <= 280) {
 			double percentual = (salario * 20) / 100;
-			System.out.println("O sal·rio antes do reajuste era: R$ " + salario);
+			System.out.println("O sal√°rio antes do reajuste era: R$ " + salario);
 			System.out.println("O percentual aplicado foi de 20%.");
 			System.out.println("O valor do aumento foi de: R$ " + percentual);
-			System.out.println("O sal·rio integrado ao aumento ficou: R$ " + (percentual + salario));
+			System.out.println("O sal√°rio integrado ao aumento ficou: R$ " + (percentual + salario));
 		} else if (salario > 280 && salario <= 700) {
 			double percentual = (salario * 15) / 100;
-			System.out.println("O sal·rio antes do reajuste era: R$ " + salario);
+			System.out.println("O sal√°rio antes do reajuste era: R$ " + salario);
 			System.out.println("O percentual aplicado foi de 15%.");
 			System.out.println("O valor do aumento foi de: R$ " + percentual);
-			System.out.println("O sal·rio integrado ao aumento ficou: R$ " + (percentual + salario));
+			System.out.println("O sal√°rio integrado ao aumento ficou: R$ " + (percentual + salario));
 		} else if (salario > 700 && salario <= 1500) {
 			double percentual = (salario * 10) / 100;
-			System.out.println("O sal·rio antes do reajuste era: R$ " + salario);
+			System.out.println("O sal√°rio antes do reajuste era: R$ " + salario);
 			System.out.println("O percentual aplicado foi de 10%.");
 			System.out.println("O valor do aumento foi de: R$ " + percentual);
-			System.out.println("O sal·rio integrado ao aumento ficou: R$ " + (percentual + salario));
+			System.out.println("O sal√°rio integrado ao aumento ficou: R$ " + (percentual + salario));
 		} else {
 			double percentual = (salario * 5) / 100;
-			System.out.println("O sal·rio antes do reajuste era: R$ " + salario);
+			System.out.println("O sal√°rio antes do reajuste era: R$ " + salario);
 			System.out.println("O percentual aplicado foi de 5%.");
 			System.out.println("O valor do aumento foi de: R$ " + percentual);
-			System.out.println("O sal·rio integrado ao aumento ficou: R$ " + (percentual + salario));
+			System.out.println("O sal√°rio integrado ao aumento ficou: R$ " + (percentual + salario));
 		}
 
 		in.close();
@@ -41,13 +41,13 @@ public class Ex32AumentoSalario {
 
 }
 /*
- * 6) As OrganizaÁıes Tabajara resolveram dar um aumento de sal·rio aos seus
- * colaboradores e lhe contrataram para desenvolver o programa que calcular· os
- * reajustes. FaÁa um programa que recebe o sal·rio de um colaborador e o
- * reajuste segundo o seguinte critÈrio, baseado no sal·rio atual: - sal·rios
- * atÈ R$ 280,00 (incluindo) : aumento de 20% - sal·rios entre R$ 280,00 e R$
- * 700,00 : aumento de 15% - sal·rios entre R$ 700,00 e R$ 1500,00 : aumento de
- * 10% - sal·rios de R$ 1500,00 em diante : aumento de 5% ApÛs o aumento ser
- * realizado, informe na tela: - sal·rio antes do reajuste; - percentual de
- * aumento aplicado; - valor do aumento; - novo sal·rio, apÛs o aumento.
+ * 6) As Organiza√ß√µes Tabajara resolveram dar um aumento de sal√°rio aos seus
+ * colaboradores e lhe contrataram para desenvolver o programa que calcular√° os
+ * reajustes. Fa√ßa um programa que recebe o sal√°rio de um colaborador e o
+ * reajuste segundo o seguinte crit√©rio, baseado no sal√°rio atual: - sal√°rios
+ * at√© R$ 280,00 (incluindo) : aumento de 20% - sal√°rios entre R$ 280,00 e R$
+ * 700,00 : aumento de 15% - sal√°rios entre R$ 700,00 e R$ 1500,00 : aumento de
+ * 10% - sal√°rios de R$ 1500,00 em diante : aumento de 5% Ap√≥s o aumento ser
+ * realizado, informe na tela: - sal√°rio antes do reajuste; - percentual de
+ * aumento aplicado; - valor do aumento; - novo sal√°rio, ap√≥s o aumento.
  */
