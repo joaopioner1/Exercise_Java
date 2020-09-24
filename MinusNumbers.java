acae81f0c4cfbbdbe455964e9a20897db869918b
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-24 14:53:42 -0300

    Adc o exercicio subtracao com vetores.

---
+++ MinusNumbers.java
@@ -0,0 +1,28 @@
+package arrays_codes;
+
+import java.util.Scanner;
+
+public class LessNumbers {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Digite quantos numeros deseja adigitar (lembre-se, o programa subtrai 2 numrs): ");
+		int count = in.nextInt();
+		double[] n1 = new double[count];
+		double[] n2 = new double[count];
+		
+		for (int i = 0; i < count; ++i) {
+			System.out.print("Insira um numero: ");
+			n1[i] = in.nextDouble();
+			System.out.print("Insira outro numero: ");
+			n2[i] = in.nextDouble();
+		}
+		
+		for (int j = 0; j < count; ++j) {
+			System.out.println("A substracao de " + n1[j] + " + " + n2[j] + " = " + (n1[j] - n2[j]));
+		}
+		in.close();
+	}
+
+}
