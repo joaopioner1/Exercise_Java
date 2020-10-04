9b63a57bbb96df7444535a213ce00bb8444cd6a6
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-04 19:54:30 -0300

    Add files via upload

--- Exercise5.java
+++ Exercise5.java
@@ -7,34 +7,17 @@ public class Exercise5 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("Calculadora com arrays");
-		System.out.print("Insira a operacao que deseja realizar(* / - / + / / /): ");
-		char operation = in.next().charAt(0);
+		double[] vet = new double[5];
 		
-		System.out.println("Insira quantos numeros deseja inserir: ");
-		int quantNum = in.nextInt();
-		double[] input = new double[quantNum];
-		
-		if (operation == '*') {
-			for (int i = 0; i < input.length; i++) {
-			System.out.print("Insira um numero para multiplicar: ");
-			input[i] = in.nextDouble();
-			}
-			
-			for (int j = 0; j < input.length; j++) {
-				input[j] *= input[j];
-				System.out.println("Produto total: " + input[j]);
-			}
-		} 
-		if (simbolo = '+') {
-			System.out.print("Insira um numero para multiplicar: ");
-			input[i] = in.nextDouble();
+		for (int i = 0; i < vet.length; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = in.nextDouble();
 		}
 		
-		
-		
-		
-		
+		System.out.println("Os numeros digitados foram: ");
+		for (int j = 0; j < vet.length; j++) {
+			System.out.println("-> " + vet[j]);
+		}
 		
 		
 		in.close();
