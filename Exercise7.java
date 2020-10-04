6877fc41fdeef4db4b56aca136462dd3514fd29b
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-04 19:55:23 -0300

    Add files via upload

---
+++ Exercise7.java
@@ -0,0 +1,23 @@
+package arrays_exercises;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		double[] notas = new double[4];
+		double soma = 0;
+		
+		for (int i = 0; i < notas.length; ++i) {
+			System.out.print("Insira uma nota: ");
+			notas[i] = in.nextDouble();
+			soma += notas[i];
+		}
+		System.out.println("A media das notas eh: " + (soma / 4));
+		
+		in.close();
+	}
+
+}
