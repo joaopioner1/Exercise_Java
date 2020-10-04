d22278e42a8d861a5228a5c2d14344f33294f355
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-04 19:56:18 -0300

    Update Exercise6.java

--- Exercise6.java
+++ Exercise6.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class Exercise6 {
 
 	public static void main(String[] args) {
-		Scanner ent = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
         
-        // declaração de vetor
+        // declaraÃ§Ã£o de vetor
         int[] vetor = new int[10];
         
         // entrada de dados
         for(int i = 0; i < vetor.length; i++){
-            System.out.println("Digite um número");
-            vetor[i] = ent.nextInt();
+            System.out.print("Digite um nÃºmero");
+            vetor[i] = in.nextInt();
         }
         
-        // saída de dados
+        // saÃ­da de dados
         for(int i = vetor.length-1; i >= 0; i--){
             System.out.println(vetor[i]);
         }
 		
-		ent.close();
+	in.close();
 	}
 
 }
