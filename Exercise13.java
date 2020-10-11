5eb703100ee1e4a6a3f19f8ffc93330b913ee84a
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-11 18:45:19 -0300

    Add files via upload

---
+++ Exercise13.java
@@ -0,0 +1,23 @@
+package arrays_exercises;
+
+import java.util.Scanner;
+
+public class Exercise13 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int[] A = new int[10];
+		
+		for (int i) {
+			
+		}
+		
+		
+		in.close();
+	}
+
+}
+/* Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre
+a soma dos quadrados dos elementos do vetor.
+*/
