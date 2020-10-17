cda81bcf5c1a3f38c3acf907dcac8563e1b6181e
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-17 17:46:17 -0300

    Add files via upload

---
+++ Exercise16.java
@@ -0,0 +1,18 @@
+package arrays_exercises;
+
+import java.util.Scanner;
+
+public class Exercise16 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("");
+		
+		
+		
+		
+		in.close();
+	}
+
+}
