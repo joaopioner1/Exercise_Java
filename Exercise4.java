8dea1ef79a04cef83144cd78fdcfc09f7997affa
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-19 19:42:47 -0300

    teste1

--- Exercise4.java
+++ Exercise4.java
@@ -8,21 +8,22 @@ public class Exercise4 {
 		Scanner in = new Scanner(System.in);
 		
 		String[] vet = new String[10];
+		//String[] vet2 = new String[vet1.length];
 		
 		for (int i = 0; i < vet.length; ++i) {
 			System.out.print("Insira um nome: ");
 			vet[i] = in.next();
 		}
 		
-		System.out.print("\nInsira um numero qualquer: ");
+		System.out.println("Insira um numero qualquer: ");
 		String name = in.next();
 		
 		for (int i = 0; i < vet.length;) {
 			if (vet[i].equals(name)) {
-				System.out.print("ACHEI O NOME");
+				System.out.print("ACHEI");
 				break;
 			} else {
-				System.out.print("NAO ACHEI O NOME");
+				System.out.print("NAO ACHEI");
 				break;
 			}
 		}
@@ -31,7 +32,7 @@ public class Exercise4 {
 	}
 
 }
-/*Ler um vetor com 10 nomes de pessoas, apÃ³s pedir que o usuÃ¡rio digite um nome qualquer de
-pessoa. Escrever a mensagem â€œACHEIâ€, se o nome estiver armazenado no vetor C ou â€œNÃƒO ACHEIâ€
-caso contrÃ¡rio. 
+/*Ler um vetor com 10 nomes de pessoas, após pedir que o usuário digite um nome qualquer de
+pessoa. Escrever a mensagem “ACHEI”, se o nome estiver armazenado no vetor C ou “NÃO ACHEI”
+caso contrário. 
 */
