66dd4744520d650a0430e2eed55364af3806429c
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-20 17:20:11 -0300

    Update Exercise17.java

--- Exercise17.java
+++ Exercise17.java
@@ -8,6 +8,7 @@ public class Exercise17 {
 		Scanner in = new Scanner(System.in);
 		
 		double[] array = new double[10];
+		short imp = 0, par = 0;
 		
 		for (int i = 0; i < array.length; i++) {
 			System.out.print("Digite um numero: ");
@@ -16,9 +17,17 @@ public class Exercise17 {
 		
 		for (int j = 0; j < array.length; j++) {
 			if (array[j] % 2 == 0) {
-				System.out.println();
+				par += 1;
+				System.out.println("Par: " + array[j]);
+			} else {
+				imp += 1;
+				System.out.println("impar: " + array[j]);
+
 			}
 		}
+		System.out.println("Quantidade n pares: " + par);
+		System.out.println("Quantidade n impares: " + imp);
+
 		
 		in.close();
 	}
